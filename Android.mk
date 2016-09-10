c79622f9712cc79f5960ed9b7a52cd76f91c66c7
Calin <nilac8991@gmail.com>
2016-09-10 18:07:28 +0200

    Initial bring up of the XOSP Equalizer
    Mega squash all my previous commits and make the repository public
    
    Signed-off-by: Calin <nilac8991@gmail.com>

--- Android.mk
+++ Android.mk
@@ -7,8 +7,10 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_JAVA_LIBRARIES := framework
 
-LOCAL_PACKAGE_NAME := MusicFX
+LOCAL_PACKAGE_NAME := Equalizer
 
 LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
